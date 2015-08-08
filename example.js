cdc5e328172d1f801ab8cd3795f37b0e6360029d
Steven Cassidy <steven@mumsnet.com>
2015-08-07 22:42:01 -0400

    Add some explanatory comments

--- example.js
+++ example.js
@@ -11,12 +11,24 @@ function exampleCallback(results, status) {
 }
 
 function showPosition(position) {
-var service = new google.maps.places.PlacesService(document.getElementById('map'));
-var request = {location: position, radius:2000};
 
-service.nearbySearch(request, exampleCallback);
+  // first construct the "service"
+  var service = new google.maps.places.PlacesService(document.getElementById('map'));
+
+  // now create a 'request' object - specifying where to search and what radius
+  // a literal like {location:{lat:42, lng:-72}} would work equally well here
+  var request = {location: position, radius:2000};
+
+  /*
+   now ask for the search and provide a callback function for the results.
+   It will get called with the results and an indication of the status. Example above.
+   In your case it will construct the params to send into your create request, 
+   as we discussed earlier.
+  */
+  service.nearbySearch(request, exampleCallback);
 }
 
+// This is just an example document.ready to show the showPosition working
 $(document).ready(function(){
   var samplePosition = {lat:42, lng:-72};
   showPosition(samplePosition);
